e30bbf6dd664e9fe65494354f9a83e7dd6292857
deconnectee07 <147107906+deconnectee07@users.noreply.github.com>
2023-10-26 17:37:11 -0400

    Add files via upload

---
+++ shaheer.py
@@ -0,0 +1,22 @@
+programme="bonjour, je suis étudiante a l'uqam"
+curseur=0
+position=phrase.find(",",curseur)
+while position>0:
+    avant=position-1
+    apres=position+1
+    if proramme[avant]!= " " and (position==len(paragraphe) -1 or paragraphe[ apres ]== " "):
+        print("ici:", paragraphe[avant-2: apres+2], "le point est OK")
+    else:
+phrase_nospace= paragraphe.replace(""," "), split(".")
+phrase_spaceparagraphe.split(".")
+if paragraphe!= len(paragraphe)
+    for index, phrase in paragraphe(phrase_nospace):
+        if phrase== phrase.capitalize[]:
+            statut=("Respecte la regle de la majuscule")
+        else:
+            statut=("Ne respecte pas la regle de la majuscule")
+        print("La phrase", (index),(phrase_space[index]), ", statut)
+else:
+    print("La regle de point n'est pas respecté")
+
+
